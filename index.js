979933742b8c8d157660661b7c727f8a0ac69d0c
pc-home <siyandamakhalima.1@gmail.com>
2018-10-02 05:24:15 +0200

    stashing

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ if (process.env.DATABASE_URL && !local){
     useSSL = true;
 }
 // which db connection to use
-const connectionString = process.env.DATABASE_URL || 'postgresql://coder:pg123@localhost:5432/shoe_api_db';
+const connectionString = process.env.DATABASE_URL || 'postgresql://tasiya:pg123@localhost:5432/shoe_api_db';
 
 const pool = new Pool({
     connectionString,
