a5d23038cf1c4ef12323c18ed6e987a5c84b1b58
tasiya <siyandamakhalima.1@gmail.com>
2018-10-22 16:46:40 +0200

    removed the config files

--- index.js
+++ index.js
@@ -1,67 +1,9 @@
-const express = require('express');
-const exphbs = require('express-handlebars');
-const bodyParser = require('body-parser');
-const session = require('express-session');
-const flash = require('express-flash');
-const pg = require("pg");
-
-const ShoeApi = require('./api/shoe_api');
-const shoeServices = require('./services/shoeServices');
-
-const Pool = pg.Pool;
-
-const app = express();
-
-app.use(session({
-    secret: 'keyboard cat',
-    resave: false,
-    saveUninitialized: true
-}));
-
-app.use(flash());
-
-app.use(bodyParser.urlencoded({
-    extended: false
-}))
-// parse application/json
-app.use(bodyParser.json())
-
-app.use(express.static('public'));
-
-// should we use a SSL connection
-let useSSL = false;
-let local = process.env.LOCAL || false;
-if (process.env.DATABASE_URL && !local) {
-    useSSL = true;
-}
-// which db connection to use
-const connectionString = process.env.DATABASE_URL || 'postgresql://tasiya:pg123@localhost:5432/shoe_api_db';
-
-const pool = new Pool({
-    connectionString,
-    ssl: useSSL
-});
-
-const service = shoeServices(pool);
-const api = ShoeApi(service);
-
-// API  
-app.get('/api/stock', api.getAll);
-app.get('/api/default', api.dropDowns);
-app.get('/api/cart', api.cartSection);
-
-app.post('/api/add', api.addStock);
-app.get('/api/stock/:id', api.addCart)
-app.get('/api/clearCart', api.deleteCart);
-app.get('/api/clearStock', api.deleteStock);
-app.get('/api/remove/:id', api.removeItemCart);
-app.get('/api/filter/brand/:brand', api.filterByBrand);
-app.get('/api/filter/colour/:colour', api.filterByColour);
-app.get('/api/filter/size/:size', api.filterBySize);
-app.get('/api/filter/stock/:stock', api.filterByStock);
-
-
+const appWebRoutes = require('./app-web-routes');
+const app = require('./config-server');
+const pool = require('./db-config');
+let server = app();
+let db_pool = pool();
+let application = appWebRoutes(server, db_pool);
 const PORT = process.env.PORT || 2018;
-app.listen(PORT, function () {
-    console.log('Listening to port...' + PORT);
-});
+server.listen(PORT, function () {console.log('Listening to port...' + PORT);});
+module.exports = application ;
