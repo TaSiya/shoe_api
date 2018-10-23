5b1be2ebcef77c8401905a769a3df9ce61a2eb59
pc-home <siyandamakhalima.1@gmail.com>
2018-10-23 06:44:18 +0200

    removed the client-side files

--- public/app.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function(){
-    let messageElem = document.querySelector('.message');
-    if (messageElem.innerHTML !== ''){
-        setTimeout(function(){
-            messageElem.innerHTML = '';
-        }, 3000);
-    }
-
-});
