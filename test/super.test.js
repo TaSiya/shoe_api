688000812fa869e75ad25f8a908650f5ac2fb326
tasiya <siyandamakhalima.1@gmail.com>
2018-11-02 16:24:10 +0200

    trying to fix the super

--- test/super.test.js
+++ test/super.test.js
@@ -1,136 +1,132 @@
-// const request = require('supertest');
-// const assert = require("assert");
-// const appWebRoutes = require('../app-web-routes');
-// const express = require('express');
-// const db = require('../db-config');
-// const dbtest = require('../dbTester');
+const request = require('supertest');
+const assert = require("assert");
+const appWebRoutes = require('../app-web-routes');
+const express = require('express');
+const db = require('../db-config');
+const dbtest = require('../dbTester');
 
-// let app = null;
-// let pool = null;
+let app = null;
+let pool = null;
 
-// describe('GET /api/stock', function() {
-//     before(function() {
-//         app = express();
-//         pool = dbtest();
-//         appWebRoutes(app, pool);
+describe('GET /api/stock', function() {
+    before(function() {
+        app = express();
+        pool = dbtest();
+        appWebRoutes(app, pool);
 
-//     });
+    });
 
     
 
-//     it('Count the number of shoes', function(done) {
-//       request(app)
-//         .get('/api/stock')
-//         .set('Accept', 'application/json')
-//         .expect(200)
-//         .then((response, err) => {
-//             let data = response.body.result;
-//             assert.strictEqual(data.length, 6);
-//             done(err)
-//         })
-//         .catch(err => {
-//             done(err)
-//         })
-//     })
+    it('Count the number of shoes', function(done) {
+      request(app)
+        .get('/api/stock')
+        .set('Accept', 'application/json')
+        .expect(200)
+        .then((response, err) => {
+            let data = response.body.result;
+            assert.strictEqual(data.length, 6);
+            done(err)
+        })
+        .catch(err => {
+            done(err)
+        })
+    })
 
-//     it('calculate number of colours in database', function (done) {
-//         request(app)
-//             .get('/api/default')
-//             .set('Accept', 'application/json')
-//             .expect(200)
-//             .then(
-//                 (response, err) =>
-//                     {
-//                         let data = response.body;
-//                         let colourData = data.colours;
-//                         assert.strictEqual(colourData.length, 7);
-//                         done(err)
-//                     }
-//             )
-//             .catch(err =>{
-//                 done(err)
-//             })
-//     })
+    it('calculate number of colours in database', function (done) {
+        request(app)
+            .get('/api/default')
+            .set('Accept', 'application/json')
+            .expect(200)
+            .then(
+                (response, err) =>
+                    {
+                        let data = response.body;
+                        let colourData = data.colours;
+                        assert.strictEqual(colourData.length, 7);
+                        done(err)
+                    }
+            )
+            .catch(err =>{
+                done(err)
+            })
+    })
 
-//     it('calculate number of brands in database', function (done) {
-//         request(app)
-//             .get('/api/default')
-//             .set('Accept', 'application/json')
-//             .expect(200)
-//             .then(
-//                 (response, err) =>
-//                     {
-//                         let data = response.body;
-//                         let brandData = data.brands;
-//                         assert.strictEqual(brandData.length, 6);
-//                         done(err)
-//                     }
-//             )
-//             .catch(err =>{
-//                 done(err)
-//             })
-//     })
+    it('calculate number of brands in database', function (done) {
+        request(app)
+            .get('/api/default')
+            .set('Accept', 'application/json')
+            .expect(200)
+            .then(
+                (response, err) =>
+                    {
+                        let data = response.body;
+                        let brandData = data.brands;
+                        assert.strictEqual(brandData.length, 6);
+                        done(err)
+                    }
+            )
+            .catch(err =>{
+                done(err)
+            })
+    })
 
-//     it('Get current sizes in database', function (done) {
-//         request(app)
-//             .get('/api/default')
-//             .set('Accept', 'application/json')
-//             .expect(200)
-//             .then(
-//                 (response, err) =>
-//                     {
-//                         let data = response.body;
-//                         let sizeData = data.size;
-//                         assert.strictEqual(sizeData.length, 4);
-//                         done(err)
-//                     }
-//             )
-//             .catch(err =>{
-//                 done(err)
-//             })
-//     })
+    it('Get current sizes in database', function (done) {
+        request(app)
+            .get('/api/default')
+            .set('Accept', 'application/json')
+            .expect(200)
+            .then(
+                (response, err) =>
+                    {
+                        let data = response.body;
+                        let sizeData = data.size;
+                        assert.strictEqual(sizeData.length, 4);
+                        done(err)
+                    }
+            )
+            .catch(err =>{
+                done(err)
+            })
+    })
 
-//     it('Get current stock in database', function (done) {
-//         request(app)
-//             .get('/api/default')
-//             .set('Accept', 'application/json')
-//             .expect(200)
-//             .then(
-//                 (response, err) =>
-//                     {
-//                         let data = response.body;
-//                         let stockData = data.stock;
-//                         assert.strictEqual(stockData.length, 5);
-//                         done(err)
-//                     }
-//             )
-//             .catch(err =>{
-//                 done(err)
-//             })
-//     })
-//     after(function(){
-//         app.stop();
-//       });
+    it('Get current stock in database', function (done) {
+        request(app)
+            .get('/api/default')
+            .set('Accept', 'application/json')
+            .expect(200)
+            .then(
+                (response, err) =>
+                    {
+                        let data = response.body;
+                        let stockData = data.stock;
+                        assert.strictEqual(stockData.length, 5);
+                        done(err)
+                    }
+            )
+            .catch(err =>{
+                done(err)
+            })
+    })
     
-//   });
+  });
 
-// describe('Testing the cart', function () {
-//     it('number items in the cart', done => {
-//         request(app)
-//             .get('/api/stock/'+1)
-//             .get('/api/cart')
-//             .set('Accept', 'application/json')
-//             .expect(200)
-//             .then(
-//                 (response, err) => {
-//                     let data = response.body;
-//                     let cartData = data.cart;
-//                     assert.strictEqual(cartData.length, 1)
-//                     done(err)
-//                 }
-//             )
-//             .catch(err =>{
-//                 done(err)
-//             })
-//     })
-// })
+describe('Testing the cart', function () {
+    it('number items in the cart', done => {
+        request(app)
+            .get('/api/cart')
+            .set('Accept', 'application/json')
+            .expect(200)
+            .then(
+                (response, err) => {
+                    let data = response.body;
+                    let cartData = data.cart;
+                    assert.strictEqual(cartData.length, 1)
+                    done(err)
+                }
+            )
+            .catch(err =>{
+                done(err)
+            })
+    })
+})
